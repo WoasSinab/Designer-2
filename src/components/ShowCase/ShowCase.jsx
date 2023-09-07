8f4c6934f103ae01c0468e3feaa69f85d9d2e92b
Sina Beyki <mrsaher83@gmail.com>
2023-09-07 22:54:01 +0330

    some optimized

--- src/components/ShowCase/ShowCase.jsx
+++ src/components/ShowCase/ShowCase.jsx
@@ -8,19 +8,19 @@ const ShowCase = () => {
       <div className='flex lg:flex-row flex-col'>
         {/* Image 1 */}
         <span className='relative md:left-16 z-10 transform md:-translate-x-8 -rotate-12'>
-          <img className='w-[300px] h-[300px] rounded-lg   transform ' src='https://s6.uupload.ir/files/neelesh-aloha_m2r.png' alt='' />
+          <img className='sm:w-[300px] sm:h-[300px] h-[200px] w-[200px] rounded-lg   transform ' src='https://s6.uupload.ir/files/neelesh-aloha_m2r.png' alt='' />
         </span>
         {/* Image 2 */}
         <span className='relative md:left-4 -top-16 lg:top-0 z-20 transform md:-translate-x-4 shadow-2xl'>
-          <img className=' w-[300px] h-[300px] rounded-lg   transform rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_41_r6pr.png' alt='' />
+          <img className=' sm:w-[300px] sm:h-[300px] h-[200px] w-[200px] rounded-lg   transform rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_41_r6pr.png' alt='' />
         </span>
         {/* Image 3 */}
         <span className='relative md:right-4 -top-24 lg:top-0 z-30 transform md:-translate-x-2 shadow-2xl'>
-          <img className='w-[300px] h-[300px] rounded-lg shadow-2xl transform -rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_17_brt8.png' alt='' />
+          <img className='sm:w-[300px] sm:h-[300px] h-[200px] w-[200px] rounded-lg shadow-2xl transform -rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_17_brt8.png' alt='' />
         </span>
         {/* Image 4 */}
         <span className='relative md:right-16 -top-32 lg:top-0 z-40 shadow-2xl'>
-          <img className='w-[300px] h-[300px] rounded-lg  shadow-2xl transform rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_55_vh3v.png' alt='' />
+          <img className='sm:w-[300px] sm:h-[300px] h-[200px] w-[200px] rounded-lg  shadow-2xl transform rotate-12' src='https://s6.uupload.ir/files/instagram_post_-_55_vh3v.png' alt='' />
         </span>
       </div>
     </div>
