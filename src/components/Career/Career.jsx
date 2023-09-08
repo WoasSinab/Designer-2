7424c1bb4333e8e8cb309cc018aabf593b99144e
Sina Beyki <mrsaher83@gmail.com>
2023-09-08 19:48:19 +0330

    add comments

--- src/components/Career/Career.jsx
+++ src/components/Career/Career.jsx
@@ -1,28 +1,40 @@
+
+// Define the Career component
 const Career = () => {
   return (
+    // Container for the career section with flex layout
     <div className='flex md:flex-row flex-col justify-start text-white lg:mx-24 mx-4 lg:my-24 my-16 gap-y-16'>
+      {/* Left column for career information */}
       <div className='md:w-1/2 text-center md:text-left'>
-        <h1 className='text-3xl font-semibold my-8'>Career SO FAR</h1>
+        <h1 className='text-3xl font-semibold my-8'>Career SO FAR 🚀</h1>
+        {/* Career description */}
         <p className='text-base text-gray-500 xl:pr-40 md:pr-20 leading-10'>I'm a Head of Design at Kenko Health with over 7 years of IT industry experience. I began as a Visual Designer at Chromeinfotech in 2018 and later worked as a UX and Visual Designer at Nickefox. My experience also includes roles as a Product Designer at Zomato and a UX Designer at Deloitte.</p>
       </div>
+      {/* Right column for skills */}
       <div className='md:w-1/2 text-center md:pl-8'>
-        <h1 className='text-3xl font-semibold my-8'>Skills</h1>
-        <div className='flex flex-col  gap-y-12 gap-x-4 mx-4'>
-          <div className='flex flex-row justify-between  items-center '>
+        <h1 className='text-3xl font-semibold my-8'>Skills 🛠️</h1>
+        {/* Skills section with flex layout */}
+        <div className='flex flex-col gap-y-12 gap-x-4 mx-4'>
+          {/* UI (User Interface) skill */}
+          <div className='flex flex-row justify-between items-center'>
             <span>
               <h1 className='md:text-2xl text-xl'>User Interface</h1>
             </span>
+            {/* Radial progress bar for UI skill */}
             <div className='radial-progress' style={{ "--value": 85 }}>
               85%
             </div>
           </div>
-          <hr className="opacity-10 w-full"/>
-          <div className='flex flex-row justify-between  items-center '>
+          {/* Horizontal line separator */}
+          <hr className='opacity-10 w-full' />
+          {/* UX (User Experience) skill */}
+          <div className='flex flex-row justify-between items-center'>
             <span>
               <h1 className='md:text-2xl text-xl'>User Experience</h1>
             </span>
+            {/* Radial progress bar for UX skill */}
             <div className='radial-progress' style={{ "--value": 75 }}>
-              65%
+              75%
             </div>
           </div>
         </div>
@@ -31,4 +43,5 @@ const Career = () => {
   );
 };
 
+// Export the Career component
 export default Career;
--- src/components/CaseStudy/CaseStudy.jsx
+++ src/components/CaseStudy/CaseStudy.jsx
@@ -1,35 +1,48 @@
 
 
+// Define the CaseStudy component
 const CaseStudy = () => {
   return (
+    // Container for the Case Study section with flex layout and spacing
     <div className="flex flex-col gap-16 lg:mx-24 mx-4 lg:my-24 my-16">
       {/* Section for Title and Icons */}
       <div className='w-full xl:text-6xl sm:text-3xl lg:text-5xl md:text-4xl text-3xl flex md:flex-row flex-col gap-6 items-center justify-center'>
+        {/* Title for the section */}
         <h1>WORK</h1>
+        {/* Icon for the section */}
         <img src='https://s6.uupload.ir/files/subtract_yw6o.png' alt='' className='w-[50px] h-[50px]' />
+        {/* Title for the section */}
         <h1>RESOURCES</h1>
+        {/* Icon for the section */}
         <img src='https://s6.uupload.ir/files/subtract_yw6o.png' alt='' className='w-[50px] h-[50px]' />
+        {/* Title for the section */}
         <h1>ILLUSTRATIONS</h1>
       </div>
 
       {/* Section for Description */}
       <div>
+        {/* Description text */}
         <p className="xl:mx-56 my-6 text-gray-500">
           Discover My Product Case Studies on Zomato, Deloitte, Freelance Projects, and Download Stunning UI Resources such as Illustrations, Design Systems, Icons, and More – All for Free!
         </p>
-        <p className="text-center mb-16 text-gray-500">Unleash Your Creativity Today!</p>
+        {/* Additional description */}
+        <p className="text-center mb-16 text-gray-500">Unleash Your Creativity Today! 🚀</p>
 
         {/* Grid for Images */}
         <div className='grid sm:grid-cols-2 grid-cols-1 gap-4 xl:mx-56'>
+          {/* Individual image */}
           <span>
             <img src='https://s6.uupload.ir/files/atlas-design-system_f2wj.png' alt='' />
           </span>
+          {/* Individual image */}
           <span>
             <img src='https://s6.uupload.ir/files/case-studies_0dz0.png' alt='' />
           </span>
+          {/* Individual image */}
           <span>
             <img src='https://s6.uupload.ir/files/dribbble-shots_qqqt.png' alt='' />
           </span>
+          {/* Individual image */}
           <span>
             <img src='https://s6.uupload.ir/files/figma-community_li2v.png' alt='' />
           </span>
@@ -39,4 +52,5 @@ const CaseStudy = () => {
   );
 };
 
+// Export the CaseStudy component
 export default CaseStudy;
