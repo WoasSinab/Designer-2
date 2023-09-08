7f56f55fd39e4f8a70af78836e6a954baf4f4a9f
Sina Beyki <mrsaher83@gmail.com>
2023-09-08 12:41:54 +0330

    Step and data created and optimied.

---
+++ src/components/Steps/Steps.jsx
@@ -0,0 +1,36 @@
+import { stepData, imgdata } from "./data";
+
+const Steps = () => {
+  return (
+    <div className='flex flex-col gap-16 lg:mx-24 mx-4 lg:my-24 my-16'>
+      {/* Section Title */}
+      <h1 className='font-semibold text-center md:text-2xl text-xl'>MY 4 STEPS CREATIVE PROCESS AND TOOLS I USED 🚀</h1>
+      {/* Steps Grid */}
+      <div className='grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4'>
+        {stepData.map((item) => (
+          <div className='bg-[#0B0B0B] px-8 pb-8 pt-6' key={item.id}>
+            {/* Step Image */}
+            <div>
+              <img src={item.img} alt='' className='w-[70px] h-[70px]' />
+            </div>
+            {/* Step Title */}
+            <h1 className='text-2xl mt-12 mb-8'>{item.title}</h1>
+            {/* Step Description */}
+            <p className='text-sm text-gray-500 leading-loose'>{item.des}</p>
+          </div>
+        ))}
+      </div>
+      {/* Tool Icons Grid */}
+      <div className='grid xl:grid-cols-8 sm:grid-cols-4 grid-cols-4 gap-8 sm:gap-16'>
+        {imgdata.map((item) => (
+          <div className="flex items-center justify-center" key={item.id}>
+            {/* Tool Icon */}
+            <img src={item.img} alt='' className="w-[45px] h-[50px]"/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Steps;
---
+++ src/components/Steps/data.js
@@ -0,0 +1,65 @@
+
+
+export const stepData = [
+  {
+    id: 1,
+    img: "https://s6.uupload.ir/files/ellipse_1881_ker.png",
+    title : "Understanding Brief",
+    des: "Understanding the brief is paramount for me. It sets the stage, outlining client goals, user needs, constraints, and brand guidelines."
+  },
+  {
+    id: 2,
+    img: "https://s6.uupload.ir/files/ellipse_1881_pq91.png",
+    title : "Research and Brainstorming",
+    des: "Research and brainstorming are essential components of my design process."
+  },
+  {
+    id: 3,
+    img: "https://s6.uupload.ir/files/ellipse_1881_0mjp.png",
+    title : "Design and Iterations",
+    des: "I create initial concepts, iterate based on feedback, and refine the product. This process ensures a user-friendly and successful design"
+  },
+  {
+    id: 4,
+    img: "https://s6.uupload.ir/files/ellipse_1881_xmqb.png",
+    title : "Dev Hand-Off and Testing",
+    des: "I provide detailed design specifications to developers, ensuring accurate implementation."
+  }
+];
+
+
+
+export const imgdata = [
+    {
+      id: 1,
+      img:"https://s6.uupload.ir/files/figma_logo_fnt7.png"
+    },
+    {
+      id: 2,
+      img:"https://s6.uupload.ir/files/image_10_cu8w.png"
+    },
+    {
+      id: 3,
+      img:"https://s6.uupload.ir/files/image_12_nv8.png"
+    },
+    {
+      id: 4,
+      img:"https://s6.uupload.ir/files/image_14_xth5.png"
+    },
+    {
+      id: 5,
+      img:"https://s6.uupload.ir/files/image_13_10yd.png"
+    },
+    {
+      id: 6,
+      img:"https://s6.uupload.ir/files/image_15_qepm.png"
+    },
+    {
+      id: 7,
+      img:"https://s6.uupload.ir/files/image_16_v512.png"
+    },
+    {
+      id: 8,
+      img:"https://s6.uupload.ir/files/image_17_8eyh.png"
+    }
+]
