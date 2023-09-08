e176ea9267aab449258074f5d0aad1a511dd084f
Sina Beyki <mrsaher83@gmail.com>
2023-09-08 14:31:57 +0330

    add comments to code

--- src/components/CaseStudy/CaseStudy.jsx
+++ src/components/CaseStudy/CaseStudy.jsx
@@ -1,6 +1,9 @@
+
+
 const CaseStudy = () => {
   return (
     <div className="flex flex-col gap-16 lg:mx-24 mx-4 lg:my-24 my-16">
+      {/* Section for Title and Icons */}
       <div className='w-full xl:text-6xl sm:text-3xl lg:text-5xl md:text-4xl text-3xl flex md:flex-row flex-col gap-6 items-center justify-center'>
         <h1>WORK</h1>
         <img src='https://s6.uupload.ir/files/subtract_yw6o.png' alt='' className='w-[50px] h-[50px]' />
@@ -9,10 +12,14 @@ const CaseStudy = () => {
         <h1>ILLUSTRATIONS</h1>
       </div>
 
+      {/* Section for Description */}
       <div>
-        <p className="xl:mx-56 my-6 text-gray-500">Discover My Product Case Studies on Zomato, Deloitte, Freelance Projects, and Download Stunning UI Resources such as Illustrations, Design Systems, Icons, and More – All for Free!</p>
+        <p className="xl:mx-56 my-6 text-gray-500">
+          Discover My Product Case Studies on Zomato, Deloitte, Freelance Projects, and Download Stunning UI Resources such as Illustrations, Design Systems, Icons, and More – All for Free!
+        </p>
         <p className="text-center mb-16 text-gray-500">Unleash Your Creativity Today!</p>
 
+        {/* Grid for Images */}
         <div className='grid sm:grid-cols-2 grid-cols-1 gap-4 xl:mx-56'>
           <span>
             <img src='https://s6.uupload.ir/files/atlas-design-system_f2wj.png' alt='' />
